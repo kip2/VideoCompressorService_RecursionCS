9c7e4fb93ebd883dc7b1a92bec71d14fd5f38844
kip2 <tqa1no4@gmail.com>
2023-10-02 21:05:43 +0900

    fix: クライアントで、serverのaddressとportをファイルから読み込むように変更

--- client/client.py
+++ client/client.py
@@ -7,14 +7,16 @@ from lib.audio_convert_menu import *
 from lib.gif_convert_menu import *
 from lib.resolution_convert_menu import *
 from lib.compression_convert_menu import *
-from lib.file_client import *
 from lib.file_select_tool import *
+from lib.tcp_client import *
 
 
 def main() -> None:
     """
         対話的にメニューを表示して、動画の変換を行う
     """
+    # client classを生成する
+    client = Client()
 
     # 必要なディレクトリを作成する
     setup_directory()
@@ -23,6 +25,7 @@ def main() -> None:
     if is_input_directory_empty():
         print("inputディレクトリが空です。inputディレクトリに変換したいファイルを設置してください。")
         sys.exit(1)
+
 
     # メニューを表示する
     select = input_loop_initial_menu()
@@ -38,20 +41,19 @@ def main() -> None:
         json_file_path, input_file_path = gif_conversion_main()
 
     # jsonファイルを送信
-    send_file_client(json_file_path)
+    client.send_file_client(json_file_path)
 
     # 変換ファイルを送信
-    send_file_client(input_file_path)
+    client.send_file_client(input_file_path)
 
     # 仕切りを表示
     print("-"*50)
 
     # 変換後のファイルを受け取る
-    recieve_file_client(OUTPUT_DIRECTORY)
+    client.recieve_file_client(OUTPUT_DIRECTORY)
 
     # jsonディレクトリを削除する
     remove_json_directory()
-
 
 def setup_directory() -> None:
     """
@@ -76,6 +78,21 @@ def remove_json_directory() -> None:
     target_dir = "json"
     shutil.rmtree(target_dir)
 
+def test_config_json():
+    filepath = "config.json"
+    d = load_json(filepath)
+    print(d)
+    print(d["webserver"]["address"])
+
+def test_class_client():
+    c = Client()
+    print(c.server_address)
+    print(c.server_port)
+    print(c.server_socket_type)
+
+
 if __name__ == "__main__":
+    # test_config_json()
+    # test_class_client()
     main()
 
--- client/lib/tcp_client.py
+++ client/lib/tcp_client.py
@@ -1,12 +1,13 @@
 import socket
+import os
 from ._header import *
-from ._address_config import *
+from .json_tool import *
 
 class TCP_Client:
     def __init__(self, server_address, server_port):
         self.server_address = server_address
         self.server_port = server_port
-        self.sock, self.addr, self.port = startup_tcp_client(self.server_address, self.server_port)
+        self.sock, self.addr, self.port = self.startup_tcp_client(self.server_address, self.server_port)
 
     def __enter__(self):
         return self
@@ -17,21 +18,94 @@ class TCP_Client:
         """
         self.sock.close()
 
-def startup_tcp_client(server_address:str, server_port: int) -> tuple:
-    """
-        TCPクライエントを起動する関数
-        connect用のsocketと、(IP_address, PORT)のタプルを返す
-        server側のアドレスを使う点に注意する
-    """
-    try:
-        sock = socket.socket(NETWORK_SOCKET_TYPE, socket.SOCK_STREAM) 
-        sock.connect((server_address, server_port))
-        return (sock, server_address, server_port)
-    except (socket.timeout, ConnectionRefusedError):
+    def startup_tcp_client(self, server_address:str, server_port: int) -> tuple:
+        """
+            TCPクライエントを起動する関数
+            connect用のsocketと、(IP_address, PORT)のタプルを返す
+        """
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            sock.connect((server_address, server_port))
+            return (sock, server_address, server_port)
+        except (socket.timeout, ConnectionRefusedError):
+            return
+        except socket.error as e:
+            print(e)
+            return 
+
+class Client():
+    def __init__(self):
+        self.config_load()
+
+    def config_load(self):
+        """
+            jsonファイルから必要な情報を読み込む
+        """
+        config_dict = load_json("config.json")
+        self.server_address = config_dict["webserver"]["address"]
+        self.server_port = config_dict["webserver"]["port"]
+
+    def recieve_file_client(self,directory_path):
+        """
+            file受け取り用のクライアント
+        """
+        with TCP_Client(self.server_address, self.server_port) as c:
+            try:
+                header = c.sock.recv(HEADER_SIZE)
+
+                # header のパース
+                filename_length, json_length, data_length = json_header_parsing(header)
+                stream_rate = 4096
+
+                filename = c.sock.recv(filename_length).decode(CHARA_CODE)
+
+                if json_length != 0:
+                    raise Exception("This data is not currently supported.")
+                if data_length == 0:
+                    raise Exception("No data to read from client.")
+
+                with open(os.path.join(directory_path, filename), "wb+") as f:
+
+                    print("recieved...", end="")
+                    while data_length > 0:
+                        data = c.sock.recv(data_length if data_length <= stream_rate else stream_rate)
+                        f.write(data)
+                        data_length -= len(data)
+
+                print()
+                print("Finished download!")
+            except Exception as e:
+                print("Error: " + str(e))
         return
-    except socket.error as e:
-        print(e)
-        return 
+
+    def send_file_client(self,filepath):
+        """
+            ファイル送信用のクライアント
+        """
+        with TCP_Client(self.server_address, self.server_port) as c:
+
+            with open(filepath, "rb") as f:
+                f.seek(0, os.SEEK_END)
+                filesize = f.tell()
+                f.seek(0,0)
+
+                if filesize > pow(2, 32):
+                    raise Exception("File must be below 2GB.")
+
+                filename = os.path.basename(f.name)
+
+                filename_bits = filename.encode(CHARA_CODE)
+
+                header = create_send_json_header(len(filename_bits), 0, filesize)
+
+                c.sock.send(header)
+                c.sock.send(filename_bits)
+
+                data = f.read(4096)
+                while data:
+                    # print("Sending...")
+                    c.sock.send(data)
+                    data = f.read(4096)
 
 def send_tcp_header(sock, header_message:str) -> None:
     """
